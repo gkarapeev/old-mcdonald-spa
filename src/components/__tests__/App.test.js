2f09e60d296b001c0a2722be833fdab752cfda94
Georgi <gkarapeev@gmail.com>
2019-07-05 17:53:31 +0300

    Add a test for the App component

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -6,6 +6,7 @@ const hist = createBrowserHistory()
 
 import App from '../App/App'
 import Nav from '../Nav/Nav'
+import Verse from '../Verse/Verse'
 import { mount } from 'enzyme'
 
 
@@ -17,4 +18,14 @@ it('contains exactly one instance of the Nav component', () => {
   )
 
   expect(wrapped.find(Nav).length).toEqual(1)
+});
+
+it('contains exactly five instances of the Verse component', () => {
+  const wrapped = mount(
+    <Router history={hist}>
+      <Route path='/' component={App} />
+    </Router>
+  )
+
+  expect(wrapped.find(Verse).length).toEqual(5)
 });
